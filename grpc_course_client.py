a4ea561c52e4a19a0e3dab1fdbd74dc99c26133b
katinagon <katinagon@yandex.ru>
2026-01-19 11:36:04 +0800

    Знакомство с gRPC протоколом. Практика работы с gRPC протоколом

---
+++ grpc_course_client.py
@@ -0,0 +1,13 @@
+import grpc
+
+import course_service_pb2
+import course_service_pb2_grpc
+
+channel = grpc.insecure_channel('localhost:50051')
+stub = course_service_pb2_grpc.CourseServiceStub(channel)
+
+response = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id="api-course"))
+output = f'''course_id: "{response.course_id}"
+title: "{response.title}"
+description: "{response.description}"'''
+print(output)
