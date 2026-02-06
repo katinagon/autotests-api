0f549394925bcb462bf027b995a70926b60b2bf6
katinagon <katinagon@yandex.ru>
2026-02-06 10:41:52 +0800

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,11 +2,13 @@ from http import HTTPStatus
 
 import pytest
 
-from clients.users.public_users_client import get_public_users_client, PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.private_users_client import PrivateUsersClient
+from clients.users.public_users_client import PublicUsersClient
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 
 
 @pytest.mark.users
@@ -18,5 +20,17 @@ def test_create_user(public_users_client: PublicUsersClient):
 
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_create_user_response(request, response_data)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(get_user_response=response_data.user, create_user_response=function_user.response.user)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
