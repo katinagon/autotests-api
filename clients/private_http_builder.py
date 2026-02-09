fe44ef027da2f8d00de0ecc2c01851e1bc8bf119
katinagon <katinagon@yandex.ru>
2026-02-09 12:22:28 +0800

    Рефакторинг и подготовка к написанию API автотестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,15 +1,20 @@
+from functools import lru_cache
+
 from httpx import Client
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
 
 
 class AuthenticationUserSchema(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
     email: str
     password: str
 
 
+@lru_cache(maxsize=None)
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     """
     Функция создаёт экземпляр httpx.Client с аутентификацией пользователя.
