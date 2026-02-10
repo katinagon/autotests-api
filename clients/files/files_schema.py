482e1e74d08b3add00f604556da960d6771f56d7
katinagon <katinagon@yandex.ru>
2026-02-10 09:42:37 +0800

    Практикуемся в написании API автотестов. Часть 1

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -27,3 +27,10 @@ class CreateFileRequestSchema(BaseModel):
     filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
     directory: str = Field(default="tests")
     upload_file: str
+
+
+class GetFileResponseSchema(BaseModel):
+    """
+    Описание структуры запроса получения файла.
+    """
+    file: FileSchema
